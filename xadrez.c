49124f63da19b90e6cfa476796f25250d34bd532
Edson Diego <124009790+d1egu1n@users.noreply.github.com>
2026-03-10 15:55:16 -0300

    Movimentando as Peças do xadrez
    Implementação da movimentação das peças de xadrez utilizando estruturas de repetição em C.
    
    - Torre: movimentação 5 casas para direita utilizando for
    - Bispo: movimentação 5 casas na diagonal (cima direita) utilizando while
    - Rainha: movimentação 8 casas para esquerda utilizando do-while
    
    O objetivo foi aplicar diferentes estruturas de repetição para simular o movimento das peças conforme o enunciado do desafio.

--- xadrez.c
+++ xadrez.c
@@ -7,16 +7,32 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+    int conttorre = 0, contbispo = 0, contrainha = 0;
+    
+    
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    printf("Bispo\n\n");
+    while (contbispo < 5)
+    {
+        printf("Cima direita\n");
+       contbispo++;
+    }
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    printf("Torre\n\n");
+    for (conttorre = 0; conttorre < 5; conttorre++)
+    {
+        printf("Direita\n");
+    }
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    printf("Rainha\n\n");
+    do
+    {
+        printf("Esquerda\n");
+        contrainha++;
+    } while (contrainha < 8);
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
